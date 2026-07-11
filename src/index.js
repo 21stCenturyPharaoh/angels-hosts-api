a54eeea72411010e608161695457985e87b98f2f
21stCenturyPharaoh <pharaoh.conglomerate.office@gmail.com>
2026-07-11 06:35:16 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 export default {
   async fetch(request, env) {
-    if (request.method !== 'POST') return new Response('Hermes-Toth API Online', {status: 200});
+    if (request.method !== 'POST') {
+      return new Response('Hermes-Toth API Online', {status: 200});
+    }
     const {to, subject, text} = await request.json();
     const res = await fetch('https://api.mailersend.com/v1/email', {
       method: 'POST',
-      headers: {'Authorization': `Bearer ${env.MAILERSENDAPI_KEYS}`, 'Content-Type': 'application/json'},
-      body: JSON.stringify({from:{email:"noreply@trial.mailersend.com"}, to:[{email:to}], subject, text})
+      headers: {
+        'Authorization': `Bearer ${env.MAILERSENDAPI_KEYS}`, 
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        from:{email:"noreply@trial.mailersend.com"}, 
+        to:[{email:to}], 
+        subject, 
+        text
+      })
     });
-    return res.ok ? Response.json({ok:true}) : new Response('Fail', {status:500});
+    return res.ok ? Response.json({ok:true}) : new Response('Mailersend Fail', {status:500});
   }
 }
